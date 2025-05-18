d24c582463c239230b9059edf06d858dd1e7a599
Sang-Chul Kim <sckim7@kookmin.ac.kr>
2025-05-18 23:31:51 +0900

    Add files via upload

---
+++ book/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from .views.fbv import book_list, book_detail
+
+urlpatterns = [
+    path('books/', book_list, name='book-list'),
+    path('books/<int:pk>/', book_detail, name='book-detail'),
+]
+
+# urls.py
+
+from .views.generic_apiview import (
+    BookListCreateView,
+    BookRetrieveView,
+    BookUpdateView,
+    BookDestroyView,
+    BookRetrieveUpdateDestroyView,
+)
+
+urlpatterns = [
+    path('generic/books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('generic/books/<int:pk>/', BookRetrieveView.as_view(), name='book-retrieve'),
+    path('generic/books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('generic/books/<int:pk>/delete/', BookDestroyView.as_view(), name='book-delete'),
+    path('generic/books/<int:pk>/detail/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
+]
